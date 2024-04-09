8c915ce98822ac36d1a471b05b2aaee6df519d94
Ivo Sitarski <sitarski02@gmail.com>
2024-04-09 15:27:37 +0300

    Added password converter.

---
+++ WPF/Converters/PasswordMaskConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class PasswordMaskConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string password)
+            {
+                return new string('*', password.Length);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported.");
+        }
+    }
+
+}
